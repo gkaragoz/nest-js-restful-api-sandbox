68130a80aff201290c9f5ec3cf14b27ac87678be
gkaragoz <gkaragoz96@gmail.com>
2020-08-12 12:37:26 +0300

    Create user module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [BooksModule],
+  imports: [BooksModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
