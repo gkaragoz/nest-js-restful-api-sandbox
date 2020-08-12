d436eb3189b75c438c6c4a54a64472e54abfe51b
gkaragoz <gkaragoz96@gmail.com>
2020-08-12 12:56:48 +0300

    Integrate User module with typegoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
 import { UserModule } from './user/user.module';
+import { TypegooseModule } from 'nestjs-typegoose';
 
 @Module({
-  imports: [BooksModule, UserModule],
+  imports: [
+    TypegooseModule.forRoot('mongodb://localhost:27017/nest-sandbox'),
+    BooksModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
